2f71f738f1ddb849d4426fea8083c44c862a50e8
Efp-BSc-inf-03-20 <efp-bsc-inf-03-20@unima.ac.mw>
2024-04-16 01:21:04 +0200

    connected to frontend

--- src/laboratory/laboratory.controller.ts
+++ src/laboratory/laboratory.controller.ts
@@ -1,3 +1,4 @@
+// laboratory.controller.ts
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LaboratoryService } from './laboratory.service';
@@ -9,61 +10,43 @@ import { UpdatedLaborotoryDTO } from './DTOs/UpdateLabotory.Dto';
 @ApiTags('Laboratory')
 export class LaboratoryController {
 
-    constructor(private LaborotoryServices: LaboratoryService) {};
+    constructor(private LaboratoryServices: LaboratoryService) {};
 
     @Post()
-
     @ApiOperation({summary:'Laboratory patient created '})
     @ApiResponse({ status: 200, description: 'Laboratory patient created successfullly ' })
     createLaborotoryPatient(@Body() LabDTO:CreateLaborotoryDTO){
-        this.LaborotoryServices.createLaborotoryPatient(LabDTO)
-        return 'Laborotory patient created sucessfully';
-
+        this.LaboratoryServices.createLaborotoryPatient(LabDTO)
+        return 'Laboratory patient created successfully';
     }
 
     @Get()
     @ApiOperation({summary:'return all Laboratory patients'})
-    @ApiResponse({ status: 200, description: 'return all Laborotary patient  ' })
-    async findAllLabotoryPatients(){
-        const patients= await this.LaborotoryServices.findAllLabotoryPatients();
-        return patients;
-  
-
+    @ApiResponse({ status: 200, description: 'return all Laboratory patient  ' })
+    async findAllLaboratoryPatients(): Promise<Laboratory[] | string> {
+        return await this.LaboratoryServices.findAllLaboratoryPatientsByCurrentDay();
     }
+
     @Get(':name')
     @ApiOperation({summary:'get an Laboratory patient  '})
-    @ApiResponse({ status: 200, description: 'an Laboratory patient returned successfullly ' })
-    async findLaborotorypatientByName(@Param('name') name: string): Promise<Laboratory[] | string> {
-      if (!name) {
-        return 'Name is not provided';
-      }
-    
-      const results = await this.LaborotoryServices.findLaborotorypatientByName(name);
-    
-      if (results.length === 0) {
-        return 'Name not found';
-      }
-    
-      return results;
+    @ApiResponse({ status: 200, description: 'an Laboratory patient returned successfully ' })
+    async findLaboratoryPatientByName(@Param('name') name: string): Promise<Laboratory[] | string> {
+        return await this.LaboratoryServices.findLaboratoryPatientByName(name);
     }
   
     @Put(':ID')
     @ApiOperation({summary:'update Laboratory patient by id'})
-    @ApiResponse({ status: 200, description: 'Laboratory patient updated successfullly ' })
-    
-    async UpdateLaborotoryPatientById(@Param('ID',ParseIntPipe) ID:number,@Body() UpadatedLabDto:UpdatedLaborotoryDTO){
-        await this.LaborotoryServices.UpdateLaborotoryPatientById(ID,UpadatedLabDto)
-        
-          return 'lab patient updated sucessfully'
-      }
+    @ApiResponse({ status: 200, description: 'Laboratory patient updated successfully ' })  
+    async UpdateLaboratoryPatientById(@Param('ID',ParseIntPipe) ID:number,@Body() UpdatedLabDto:UpdatedLaborotoryDTO){
+        await this.LaboratoryServices.UpdateLaboratoryPatientById(ID, UpdatedLabDto);
+        return 'Lab patient updated successfully';
+    }
 
     @Delete(':ID')
     @ApiOperation({summary:'Delete Laboratory patient  '})
-    @ApiResponse({ status: 200, description: 'Laboratory patient deleted successfullly ' })
-
-    DeleteLabPatientByI(@Param('ID',ParseIntPipe)ID:number){
-        this.LaborotoryServices.DeleteLabPatientById(ID);
-          return 'lab patient deleted sucessfully'
-      }
-  
+    @ApiResponse({ status: 200, description: 'Laboratory patient deleted successfully ' })
+    async DeleteLabPatientById(@Param('ID',ParseIntPipe)ID:number){
+        await this.LaboratoryServices.DeleteLabPatientById(ID);
+        return 'Lab patient deleted successfully';
+    }  
 }
--- src/laboratory/laboratory.service.ts
+++ src/laboratory/laboratory.service.ts
@@ -1,89 +1,98 @@
+// laboratory.service.ts
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Laboratory } from 'src/Entitys/Laborotary.Entity';
-import { Repository } from 'typeorm';
+import { Repository, LessThan, MoreThanOrEqual } from 'typeorm'; // Import LessThan and MoreThanOrEqual
 import { LabItem } from '../interfaces/lab-item.interface';
-import { CreateLaborotoryParams, UpdateLaborotoryParams } from './Utils/types';
+import { UpdateLaborotoryParams } from './Utils/types';
 
 @Injectable()
 export class LaboratoryService {
-    constructor(@InjectRepository(Laboratory) private LaboraotyRepository: Repository<Laboratory>){}
+    constructor(@InjectRepository(Laboratory) private LaboratoryRepository: Repository<Laboratory>) {}
 
+    async findAllLaboratoryPatientsByCurrentDay(): Promise<Laboratory[] | string> {
+        const today = new Date();
+        const startOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+        const endOfDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+    
+        const patients = await this.LaboratoryRepository.find({
+            where: {
+                Date: MoreThanOrEqual(startOfDay) && LessThan(endOfDay),
+            },
+        });
+    
+        if (patients.length === 0) {
+            return 'Oops! No data available for today.';
+        }
+    
+        return patients;
+    }
+    
+    async findLaboratoryPatientByName(FirstName?: string, LastName?: string): Promise<Laboratory[] | string> {
+        const queryBuilder = this.LaboratoryRepository.createQueryBuilder('Laboratorypatients');
+    
+        if (FirstName && LastName) {
+            queryBuilder.where('LOWER(Laboratorypatients.FirstName || Laboratorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${FirstName}${LastName}%` });
+        } else if (FirstName) {
+            queryBuilder.where('LOWER(Laboratorypatients.FirstName || Laboratorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${FirstName}%` });
+        } else if (LastName) {
+            queryBuilder.where('LOWER(Laboratorypatients.FirstName || Laboratorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${LastName}%` });
+        }
+    
+        const results = await queryBuilder.getMany();
 
-    async  findAllLabotoryPatients(): Promise<Laboratory[]>{
-        return this.LaboraotyRepository.find();
+        if (results.length === 0) {
+            return 'Name not found';
+        }
+    
+        return results;
+    }
+    
+    async createLaborotoryPatient(labItem: LabItem): Promise<void> {
+        const newPatientOnLaborotory = this.LaboratoryRepository.create({
+            FirstName: labItem.FirstName,
+            LastName: labItem.LastName,
+            PaymentMethod: labItem.PaymentMethod,
+            TestOrdered: labItem.TestOrdered,
+            Date: new Date(),
+        });
+        await this.LaboratoryRepository.save(newPatientOnLaborotory);
+    }
+    
+    async countPatients(): Promise<number> {
+        return this.LaboratoryRepository.count(); // No need for await here
+    }
+    
+    async countPatientsWithMessage(): Promise<string> {
+        const count = await this.countPatients();
+        return `This is the number of patients in laboratory today: ${count}`;
+    }
+    
+    async UpdateLaboratoryPatientById(ID: number, UpdatedLaboratoryDetails: UpdateLaborotoryParams): Promise<void> {
+        const updateObject: Partial<UpdateLaborotoryParams> = {};
+    
+        if (UpdatedLaboratoryDetails.FirstName !== undefined) {
+            updateObject.FirstName = UpdatedLaboratoryDetails.FirstName;
+        }
+    
+        if (UpdatedLaboratoryDetails.LastName !== undefined) {
+            updateObject.LastName = UpdatedLaboratoryDetails.LastName;
+        }
+    
+        if (UpdatedLaboratoryDetails.PaymentMethod !== undefined) {
+            updateObject.PaymentMethod = UpdatedLaboratoryDetails.PaymentMethod;
+        }
+    
+        if (UpdatedLaboratoryDetails.TestOrdered !== undefined) {
+            updateObject.TestOrdered = UpdatedLaboratoryDetails.TestOrdered;
+        }
+    
+        if (Object.keys(updateObject).length > 0) {
+            await this.LaboratoryRepository.update(ID, updateObject);
+        }
     }
 
-   
-    async findLaborotorypatientByName(FirstName?: string, LastName?: string): Promise<Laboratory[]> {
-      const queryBuilder = this.LaboraotyRepository.createQueryBuilder('Laborotorypatients');
-  
-      if (FirstName && LastName) {
-          queryBuilder.where('LOWER(Laborotorypatients.FirstName || Laborotorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${FirstName}${LastName}%` });
-      } else if (FirstName) {
-          queryBuilder.where('LOWER(Laborotorypatients.FirstName || Laborotorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${FirstName}%` });
-      } else if (LastName) {
-          queryBuilder.where('LOWER(Laborotorypatients.FirstName || Laborotorypatients.LastName) LIKE LOWER(:FullName)', { FullName: `%${LastName}%` });
-      }
-  
-      return queryBuilder.getMany();
-  }
-  
-  
-// Update the createLaborotoryPatient method to accept LabItem as input
-async createLaborotoryPatient(labItem: LabItem): Promise<void> {
-    const newPatientOnLaborotory = this.LaboraotyRepository.create({
-        FirstName: labItem.FirstName,
-        LastName: labItem.LastName,
-        PaymentMethod: labItem.PaymentMethod,
-        TestOrdered: labItem.TestOrdered,
-        Date: new Date(),
-    });
-    await this.LaboraotyRepository.save(newPatientOnLaborotory);
-}
-
-  async countPatients(): Promise<number> {
-    const count = await this.LaboraotyRepository.count();
-    return count;
-  }
-
-
-  async countPatientsWithMessage(): Promise<string> {
-    const count = await this.countPatients();
-    return `This is the number of patients in laborotary today: ${count}`;
-  }
-
-  async UpdateLaborotoryPatientById(ID: number, UpdatedLaborotoryDetails: UpdateLaborotoryParams): Promise<void> {
-    const updateObject: Partial<UpdateLaborotoryParams> = {};
-
-    if (UpdatedLaborotoryDetails.FirstName !== undefined) {
-        updateObject.FirstName= UpdatedLaborotoryDetails.FirstName;
+    async DeleteLabPatientById(id:number): Promise<void>{
+        await this.LaboratoryRepository.delete(id);
     }
-
-    if (UpdatedLaborotoryDetails.LastName !== undefined) {
-        updateObject.LastName = UpdatedLaborotoryDetails.LastName;
-    }
-
-    if (UpdatedLaborotoryDetails.PaymentMethod !== undefined) {
-        updateObject.PaymentMethod = UpdatedLaborotoryDetails.PaymentMethod;
-    }
-
-    if (UpdatedLaborotoryDetails.TestOrdered !== undefined) {
-        updateObject.TestOrdered = UpdatedLaborotoryDetails.TestOrdered;
-    }
-
-   
-
-    if (Object.keys(updateObject).length > 0) {
-        await this.LaboraotyRepository.update(ID, updateObject);
-    }
-}
- async DeleteLabPatientById(id:number): Promise<void>{
-    await this.LaboraotyRepository.delete(id);
-  }
-
-
-
-
-
 }
